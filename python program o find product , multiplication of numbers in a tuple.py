5d4a874799cab92ccfd0068fd026cd4d0d2c82e2
Pasamnutan <115466062+Pasamnutan@users.noreply.github.com>
2024-03-13 00:56:48 +0530

    Add files via upload

---
+++ python program o find product , multiplication of numbers in a tuple.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 00:46:06 2024
+
+@author: PASAM
+"""
+
+# Original Tuple 1
+tuple1 = (4, 3, 2, 2, -1, 18)
+product1 = 1
+for num in tuple1:
+    product1 *= num
+print("Original Tuple:")
+print(tuple1)
+print("Product - multiplying all the numbers of the said tuple:", product1)
+
+# Original Tuple 2
+tuple2 = (2, 4, 8, 8, 3, 2, 9)
+product2 = 1
+for num in tuple2:
+    product2 *= num
+print("\nOriginal Tuple:")
+print(tuple2)
+print("Product - multiplying all the numbers of the said tuple:", product2)
